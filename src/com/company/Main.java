11f9040fc3472daff4cfdd214f17a02ca2dc8dc7
automaiondineth <92194513+automaiondineth@users.noreply.github.com>
2021-10-13 16:48:33 +0530

    creating the base of the framework

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,11 @@
 package com.company;
 
+import org.openqa.selenium.WebDriver;
+
 import java.util.Scanner;
 
 public class Main {
-    public static int enterMarks() {
-        int i;
-        Scanner s = new Scanner(System.in);
-        for (i = 0; i < 7; i++) {
-            System.out.println("please enter the marks");
-            i = s.nextInt();
-
-        }
-        return i;
-    }
-
-
     public static void main(String[] args) {
-
 
     }
 }
