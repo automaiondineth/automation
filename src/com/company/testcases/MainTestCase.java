cc22f80149fd3794cbaf674647df4c1ca9be0a93
automaiondineth <92194513+automaiondineth@users.noreply.github.com>
2021-10-19 09:59:21 +0530

    configuration setup up with the files.

--- src/com/company/testcases/MainTestCase.java
+++ src/com/company/testcases/MainTestCase.java
@@ -1,15 +1,20 @@
-package com.company;
+package com.company.testcases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.Scanner;
 
-public class Main {
+public class MainTestCase {
     public static void main(String[] args) {
+
+    }
+
+    public void setup() {
         WebDriver driver = new ChromeDriver();
         System.setProperty("chromeDriver", "");
         System.getProperty("");
         driver.manage().window().maximize();
+
     }
 }
