350ddaae0b93a31aae86439779801601970551df
automaiondineth <92194513+automaiondineth@users.noreply.github.com>
2021-10-19 09:59:21 +0530

    configuration setup up with the files.

---
+++ src/com/company/testcases/testing.java
@@ -0,0 +1,11 @@
+package com.company.testcases;
+
+public class testing extends MainTestCase{
+    @Override
+    public void setup() {
+        super.setup();
+    }
+    //write the test steps
+
+
+}
